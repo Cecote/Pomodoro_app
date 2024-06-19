359a34cfeb10081462f2d5db73cfea9b871b5a3d
Cecote <thiago.cecote@aluno.ufop.edu.br>
2024-06-19 07:56:55 -0300

    Create: Config page layout finalized

--- lib/config-menu.dart
+++ lib/config-menu.dart
@@ -28,32 +28,382 @@ class PomodoroConfig extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xfff5fbff),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Column(
         children: [
           Container(
-            alignment: AlignmentDirectional.center,
-            height: 110,
-            width: 110,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.black,
-                width: 2.0,
-              ),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
+            alignment: AlignmentDirectional.topStart,
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Tempo para focar',
               style: TextStyle(
-                fontSize: 30.0,
-                fontFamily: 'Titi',
-              ), // Aumente o tamanho da fonte aqui
-              decoration: InputDecoration(
-                border: InputBorder.none, // Remove a linha preta abaixo do texto
-                contentPadding: EdgeInsets.all(10.0), // Adiciona preenchimento ao redor do texto
+                fontSize: 20,
+                fontFamily: 'Titi'
               ),
-              inputFormatters: [LengthLimitingTextInputFormatter(2)],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                alignment: AlignmentDirectional.center,
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: '00'),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                  // Aumente o tamanho da fonte aqui
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // Remove a linha preta abaixo do texto
+                    contentPadding: EdgeInsets.all(
+                        10.0), // Adiciona preenchimento ao redor do texto
+                  ),
+                  inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+                height: 50.0,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30),
+                  ':',
+                ),
+              ),
+              Container(
+                alignment: AlignmentDirectional.center,
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: '25'),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                  // Aumente o tamanho da fonte aqui
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // Remove a linha preta abaixo do texto
+                    contentPadding: EdgeInsets.all(
+                        10.0), // Adiciona preenchimento ao redor do texto
+                  ),
+                  inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+                height: 50.0,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30),
+                  ':',
+                ),
+              ),
+              Container(
+                alignment: AlignmentDirectional.center,
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: '00'),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                  // Aumente o tamanho da fonte aqui
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // Remove a linha preta abaixo do texto
+                    contentPadding: EdgeInsets.all(
+                        10.0), // Adiciona preenchimento ao redor do texto
+                  ),
+                  inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            alignment: AlignmentDirectional.topStart,
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Tempo para descansar (Curto)',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Titi'
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                alignment: AlignmentDirectional.center,
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: '00'),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                  // Aumente o tamanho da fonte aqui
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // Remove a linha preta abaixo do texto
+                    contentPadding: EdgeInsets.all(
+                        10.0), // Adiciona preenchimento ao redor do texto
+                  ),
+                  inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+                height: 50.0,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30),
+                  ':',
+                ),
+              ),
+              Container(
+                alignment: AlignmentDirectional.center,
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: '05'),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                  // Aumente o tamanho da fonte aqui
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // Remove a linha preta abaixo do texto
+                    contentPadding: EdgeInsets.all(
+                        10.0), // Adiciona preenchimento ao redor do texto
+                  ),
+                  inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+                height: 50.0,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30),
+                  ':',
+                ),
+              ),
+              Container(
+                alignment: AlignmentDirectional.center,
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: '00'),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                  // Aumente o tamanho da fonte aqui
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // Remove a linha preta abaixo do texto
+                    contentPadding: EdgeInsets.all(
+                        10.0), // Adiciona preenchimento ao redor do texto
+                  ),
+                  inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            alignment: AlignmentDirectional.topStart,
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Tempo para descansar (Longo)',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Titi'
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                alignment: AlignmentDirectional.center,
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: '00'),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                  // Aumente o tamanho da fonte aqui
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // Remove a linha preta abaixo do texto
+                    contentPadding: EdgeInsets.all(
+                        10.0), // Adiciona preenchimento ao redor do texto
+                  ),
+                  inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+                height: 50.0,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30),
+                  ':',
+                ),
+              ),
+              Container(
+                alignment: AlignmentDirectional.center,
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: '10'),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                  // Aumente o tamanho da fonte aqui
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // Remove a linha preta abaixo do texto
+                    contentPadding: EdgeInsets.all(
+                        10.0), // Adiciona preenchimento ao redor do texto
+                  ),
+                  inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+                height: 50.0,
+                child: Text(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30),
+                  ':',
+                ),
+              ),
+              Container(
+                alignment: AlignmentDirectional.center,
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: TextField(
+                  controller: TextEditingController(text: '00'),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                  // Aumente o tamanho da fonte aqui
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // Remove a linha preta abaixo do texto
+                    contentPadding: EdgeInsets.all(
+                        10.0), // Adiciona preenchimento ao redor do texto
+                  ),
+                  inputFormatters: [LengthLimitingTextInputFormatter(2)],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 50),
+            child: RoundButton(
+              text: 'Salvar',
+              onPressed: () {}
             ),
           ),
         ],
